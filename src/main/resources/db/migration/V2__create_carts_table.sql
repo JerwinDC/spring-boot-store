cac68f3e49e603706f29b46dd6fe9efb7c1d45b8
Jerwin <decastrojerwindj@gmail.com>
2025-09-10 15:11:48 +0800

    Fix V2 migration for Railway

--- src/main/resources/db/migration/V2__create_carts_table.sql
+++ src/main/resources/db/migration/V2__create_carts_table.sql
@@ -1,12 +1,30 @@
-CREATE TABLE `store_api`.`carts` (
-  `id` BINARY(16) NOT NULL,
-  `date_created` DATE NOT NULL DEFAULT (curdate()),
-  PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+-- carts table
+CREATE TABLE carts (
+  id BINARY(16) NOT NULL,
+  date_created DATE NOT NULL DEFAULT CURRENT_DATE,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
-CREATE TABLE `store_api`.`carts_item` (`id` BIGINT NOT NULL AUTO_INCREMENT , `cart_id` BINARY(16) NOT NULL , `product_id` BIGINT NOT NULL , `quantity` INT NOT NULL DEFAULT '1' , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+-- carts_item table
+CREATE TABLE carts_item (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  cart_id BINARY(16) NOT NULL,
+  product_id BIGINT NOT NULL,
+  quantity INT NOT NULL DEFAULT 1,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
-ALTER TABLE `carts_item` ADD CONSTRAINT `cart_items_carts__fk` FOREIGN KEY (`cart_id`) REFERENCES `carts`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION; ALTER TABLE `carts_item` ADD CONSTRAINT `cart_items_products__fk` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+-- foreign key to carts
+ALTER TABLE carts_item
+ADD CONSTRAINT cart_items_carts__fk
+FOREIGN KEY (cart_id) REFERENCES carts(id) ON DELETE CASCADE;
 
-ALTER TABLE `store_api`.`carts_item`
-ADD CONSTRAINT cart_items_cart_product_unique UNIQUE (`cart_id`, `product_id`);
+-- foreign key to products
+ALTER TABLE carts_item
+ADD CONSTRAINT cart_items_products__fk
+FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE;
+
+-- unique constraint
+ALTER TABLE carts_item
+ADD CONSTRAINT cart_items_cart_product_unique
+UNIQUE (cart_id, product_id);
