38245befaf8f32bb7923b5a6678186bc65f54a8e
Jerwin <decastrojerwindj@gmail.com>
2025-09-10 18:03:55 +0800

    Modified payment intent creation

--- src/main/java/com/codewithmosh/store/payments/StripePaymentGateway.java
+++ src/main/java/com/codewithmosh/store/payments/StripePaymentGateway.java
@@ -32,7 +32,7 @@ public class StripePaymentGateway implements PaymentGateway{
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                     .setCancelUrl(websiteUrl + "/checkout-cancel")
-                    .putMetadata("order_id", String.valueOf(order.getId()));
+                    .setPaymentIntentData(createPaymentIntent(order));
 
             order.getItems().forEach(items -> {
                         var lineItem = createLineItem(items);
@@ -47,6 +47,11 @@ public class StripePaymentGateway implements PaymentGateway{
             System.out.println(ex.getMessage());
             throw new PaymentException();
         }
+    }
+
+    private SessionCreateParams.PaymentIntentData createPaymentIntent(Order order) {
+        return SessionCreateParams.PaymentIntentData.builder()
+                .putMetadata("order_id", String.valueOf(order.getId())).build();
     }
 
     @Override
