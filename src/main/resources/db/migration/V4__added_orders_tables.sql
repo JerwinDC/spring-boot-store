4b83d82769bf5ff2e92019c667456b22ee7107fe
Jerwin <decastrojerwindj@gmail.com>
2025-09-10 17:03:27 +0800

    Merged V4 and V5

--- src/main/resources/db/migration/V4__added_orders_tables.sql
+++ src/main/resources/db/migration/V4__added_orders_tables.sql
@@ -1,15 +1,28 @@
-CREATE TABLE `store_api`.`orders` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `customer_id` BIGINT NOT NULL,
-  `status` VARCHAR(20) NOT NULL,
-  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `total_price` DECIMAL(10,2) NOT NULL,
-  PRIMARY KEY (`id`)
+-- orders table
+CREATE TABLE orders (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  customer_id BIGINT NOT NULL,
+  status VARCHAR(20) NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  total_price DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
-ALTER TABLE `orders` ADD CONSTRAINT `orders_users__fk` FOREIGN KEY (`customer_id`) REFERENCES `orders`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+-- correct foreign key
+ALTER TABLE orders
+ADD CONSTRAINT orders_users__fk
+FOREIGN KEY (customer_id) REFERENCES users(id)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-
-CREATE TABLE `store_api`.`order_items` (`id` BIGINT NOT NULL AUTO_INCREMENT , `order_id` BIGINT NOT NULL , `product_id` BIGINT NOT NULL , `unit_price` DECIMAL(10,2) NOT NULL , `quantity` INT NOT NULL , `total_price` DECIMAL(10,2) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-
-ALTER TABLE `order_items` ADD CONSTRAINT `orders_items_orders_id__fk` FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT; ALTER TABLE `order_items` ADD CONSTRAINT `order_items_product__fk` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+-- order_items table
+CREATE TABLE order_items (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  order_id BIGINT NOT NULL,
+  product_id BIGINT NOT NULL,
+  unit_price DECIMAL(10,2) NOT NULL,
+  quantity INT NOT NULL,
+  total_price DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT order_items_orders_fk FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT order_items_product_fk FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
